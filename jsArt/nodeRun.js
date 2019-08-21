11f8823f208a8fee1c6f3e3656bbba61cdb3e025
Rob Au <robau6@gmail.com>
2019-08-21 03:24:10 -0500

    including node

---
+++ jsArt/nodeRun.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var myParser = require("body-parser");
+
+app.use(express.static(__dirname));
+app.get('/*', function(req, res){
+  res.sendFile('index.html', { root: __dirname });
+});
+http.listen(10001, () => console.log('running...'));
