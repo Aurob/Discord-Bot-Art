8ce1848b6329256236dcb4174d15e07a0e062d47
Rob Au <robau6@gmail.com>
2019-08-22 23:10:29 -0500

    latest bug additions

--- jsArt/nodeRun.js
+++ jsArt/nodeRun.js
@@ -1,10 +1,37 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var myParser = require("body-parser");
 
 app.use(express.static(__dirname));
 app.get('/*', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
+
+var ims = {};
+function init(socket){
+  socket.emit('init',true);
+  socket.on('bndMove', function(loc, childs, id, sid){
+    (sid in ims) ? '' : ims[sid] = [];
+    if(childs[1]!='' && ims[sid].indexOf(hash(childs[1]) <0)){
+      ims[sid].push(hash(childs[1]));      
+    } 
+    //console.log(sid, id, childs[1], loc);
+    newIm = (hash(childs[1]) in ims[sid]) ? false:true;
+    io.emit('update', sid, id, childs, loc, newIm);
+  });
+}
+
+function hash(str){
+  var hash = 0, i, chr;
+  if (str.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+io.on('connection',init);
 http.listen(10001, ()=>{console.log("Running...")});
--- jsArt/script.js
+++ jsArt/script.js
@@ -1,7 +1,39 @@
+var socket = io();
+
+socket.on('init',(res)=>{
+    console.log(res);
+});
+
+var playerBnds = {};
+socket.on('update',(sid, id, childs, loc)=>{
+    
+    //console.log(sid, id, childs, loc);
+    if(sid!=socket.id){
+        
+        console.log('updating');
+        var bnd;
+        if(sid in playerBnds){
+            bnd = get(playerBnds[sid]);
+        }
+        else{
+            bnd = get(addBoundary());
+            playerBnds[sid] = bnd.id;
+        } 
+        
+        //bnd.style = style;
+        bnd.style.position = 'absolute';
+        bnd.style.left = loc[0];
+        bnd.style.top = loc[1];
+        bnd.style.width = loc[2];
+        bnd.style.height = loc[3];
+        bnd.children[1].src = childs[1];
+        // 
+    }
+
+})
 //HTML script init
 function runit(){  
     console.log("hello");
-
     //zoom();
     addBoundary(); // change name?
 }
@@ -18,50 +50,96 @@ function bndId(node){
     return id;
 }
 
+canvas = get('main');
+context = canvas.getContext('2d');
 var clicked;
 var cx = 0, cy = 0;
 var bndCanvas = [];
 var bndImages = [];
 var clickId;
 var del;
+var resizetop, resizing, dontgrab;
+var canvas, context;
+var bndClickX = 0, bndClickY = 0;
+var oobX = 0, oobY = 0;
+var scrollClick, oob;
 
+function movBound(e){
+    var b = get(clickId);
+    b.style.position ='absolute';
+    if(bndClickX != 0 && bndClickY !=0){
+        b.style.left = (e.x)-(bndClickX)+'px';//-(b.clientWidth/3)+'px';
+        b.style.top = (e.y)-(bndClickY)+'px';//-(b.clientHeight/3)+'px';
+    }
+
+    bndLoc = [b.style.left,b.style.top,b.style.width,b.style.height]
+    bndChilds = [b.children[0].innerText,b.children[1].src]
+    socket.emit('bndMove', bndLoc, bndChilds, clickId, socket.id);
+}
 function boundaryHover(e){
     //console.log("Hovering in "+e.target.id);
 
     if(del){
-        b = get(e.target.id);
+        b = get(bndId(e));
         b.remove();
         del = false;
 
     }
-    var hoverNode = bndId(e);
+    var hoverNode = (e.target.id=='') ? false : bndId(e);
 
     if(clicked && hoverNode==clickId){
-        var b = get(clickId);
-        b.style.position ='absolute';
-        b.style.left = e.x-(b.clientWidth/2)+'px';
-        b.style.top = e.y-(b.clientHeight/2)+'px';
+        movBound(e);
+    }
+
+    if(clicked && !hoverNode){
+        //console.log('oob');
+        oobX = e.clientX;
+        oobY = e.clientY;
+        oob = true;
+        movBound(e);
+    }
+    if(e.layerY < 50){
+        //console.log('resize?');
+        resizetop = true;
+    }else resizetop = false;
+
+    if(dontgrab){
+        //console.log("resizing");
+        b = get(bndId(e));
+        //console.log(b);        
     }
 }
 
 function boundaryClick(e){
     
     if(e.button != 2){
+
+
         var b = get(bndId(e));
         if(clicked){
             clicked = false;
+            bndClickX = 0;
+            bndClickY = 0;
             clickId = '';
             if(bndImages.indexOf(bndId(e)) < 0){
                 b.style.borderColor = "black";
-            } else b.style.border = '';
+                b.style.cursor = 'grab';
+            } else {
+                b.style.border = '';
+                b.style.cursor = 'grab';
+            }
             
             b.style.zIndex = "-1"; //when not moving a boundary, move it to the back
         }else {
             console.log(e); 
             clickId = bndId(e);
+            movBound(e);
             b.style.border = "solid";
             b.style.borderColor = "red";
             b.style.zIndex = "0";
+            b.style.cursor = 'grabbing';
+            bndClickX = e.layerX;
+            bndClickY = e.layerY;
             clicked = true;
         }
     }
@@ -88,6 +166,7 @@ function fileDrop(e, bg){
                 reader.onloadend = function() {
                     if(bg){
                         document.body.style.backgroundImage = "url("+reader.result+")";
+                        document.body.style.backgroundSize = 'cover';
                     }else{
                         var boundary = get(id);
                         boundary.children[1].src = reader.result;
@@ -96,6 +175,16 @@ function fileDrop(e, bg){
                         boundary.children[1].style.zIndex = "0";
                         boundary.style.border = '';
                     }
+                }
+                
+            }
+            if(files[0].type.startsWith('text')){
+                let reader = new FileReader()
+                reader.readAsText(files[0])
+                reader.onloadend = function() { 
+                    var boundary = get(id);
+                    boundary.innerText = reader.result;
+                    boundary.fontSize = boundary.width*.1+'px'; 
                 }
             }
             bndImages.push(id);
@@ -118,6 +207,10 @@ function fileDrop(e, bg){
     }
 }
 
+function bndText(e){
+    var boundary = get(clickId);
+    boundary.childNodes[1].innerText = 'text here';
+}
 function deleteBoundary(e){
     var boundary = get(clickId);
     boundary.remove();
@@ -130,28 +223,36 @@ function addBoundary(){
     var boundId = "_f"+boundCnt;
     board = get('board');
     board.innerHTML += 
-        "<div class='fz' id='"+boundId+"' style='width:25px;height:25px;'>\
+        "<div class='fz' id='"+boundId+"' style='border:solid;width:25px;height:25px;'>\
         <p contenteditable='true' style='font-size:.1px; word-wrap:break-word;'></p>\
         <img id='"+boundId+"'/>\
         </div>";
     boundaries.push(boundId);
     boundCnt++;
+    return boundId;
 }
 
 //Zoom
 var zooming = false;
 w = 0;
+var outOfBnd = false;
 function zoom(e){
-    var scrollLocation = String(e.target.id);       
+    var scrollLocation = String(e.target.id);    
     if(scrollLocation.startsWith('_f')){
         fileBoundary = get(scrollLocation);
         if(boundaries.indexOf(scrollLocation) > -1){
             bw = fileBoundary.style.width;
+            bl = fileBoundary.style.left;
+            bt = fileBoundary.style.top;
             bw = parseInt(String(bw).replace('px',''));
-            (e.deltaY < 0) ? bw+=10 : bw-=10;
+            bl = parseInt(String(bl).replace('px',''));
             
-            fileBoundary.style.width = bw+'px';
-            fileBoundary.style.height = bw+'px';
+            zm = 0;
+            (e.deltaY < 0) ? zm=10 : zm=-10;
+            
+            fileBoundary.style.width = zm+bw+'px';
+            fileBoundary.style.height = zm+bw+'px';
+
             text = fileBoundary.children[0];
             text.style.fontSize = bw*.1+'px';
             //console.log(text);
@@ -165,12 +266,19 @@ function zoom(e){
 //Scrolling
 window.addEventListener('wheel',(zoom));
 
+//window.addEventListener('mousedown', (resize));
+//window.addEventListener('mouseup', (resize));
+
 //For tracking mouse location and boundary dragging
 window.addEventListener('mousemove',(e)=> {
-    (e.target.id!='') ? boundaryHover(e) : '';
+    (e.target.id!='' || clicked) ? boundaryHover(e) : '';
+    
 }); 
 window.addEventListener('click',(e)=> {
-    (e.target.id!='') ? boundaryClick(e) : '';
+    // boundaryClick(e)
+    id = (e.target.id=='') ? '' : bndId(e);
+    (id!='' && id.startsWith('_f')) ? boundaryClick(e) : '';
+    //(e.target.id!='') ? boundaryClick(e) : (String(e).indexOf('Para') >-1) ? console.log('123') : boundaryClick(e);
 });
 
 //For handling file drops
@@ -183,4 +291,7 @@ window.addEventListener('drop', (e)=> {
 window.addEventListener('keypress',(e)=>{
     (e.key=='=') ? addBoundary(false) : '';
     (e.key=='-') ? deleteBoundary(e) : '';
+    (e.key=='t') ? bndText(e) : '';
+    
+    
 });
